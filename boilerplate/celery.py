278de2c52025be555e6a3c413ec4bcf07a7c0575
alexHooper <alexmohack@gmail.com>
2019-07-16 01:37:05 +0530

    remove duplicate code

--- boilerplate/celery.py
+++ boilerplate/celery.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, absolute_import
 
+import os
+
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boilerplate.settings')
@@ -8,14 +10,9 @@ app = Celery('boilerplate')
 
 app.conf.update(
 	task_routes={
-		'notifications.tasks.send_verify_email': {'queue': 'notifications'},
+		'notifications.tasks.send_notification_email': {'queue': 'notifications'},
 	},
 )
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 

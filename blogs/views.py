a0d23a987f208f60a69709a5048b9958ebcf87c9
alexHooper <alexmohack@gmail.com>
2019-07-15 18:33:28 +0530

    create view for home page

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Blog
+
+def home_view(request):
+	return render(request, 'blogs/home.html')
+
+def list_view(request):
+	blogs = Blog.objects.all()
+	return render(request, 'blogs/list.html', {'blogs': blog})
+
+def detail_view(request, id):
+	blog = Blog.objects.get(id=id)
+	return render(request, 'blogs/detail.html', {'blog': blog})
